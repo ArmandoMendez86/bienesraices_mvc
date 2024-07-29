5c2c85e168c4ab294b9aedd430bc3fe951898101
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2024-07-29 12:46:08 -0600

    Implementando el envio de correos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,7 @@
 import Usuario from "../models/Usuario.js";
 import { check, validationResult } from "express-validator";
+import { generarId } from "../helpers/tokens.js";
+import { emailRegistro } from "../helpers/emails.js";
 
 const formularioLogin = (req, resp) => {
   resp.render("auth/login", {
@@ -25,6 +27,8 @@ const registrar = async (req, resp) => {
   await check("password")
     .isLength({ min: 6 })
     .withMessage("El password debe tener minimo de 6 caracteres")
+    .notEmpty()
+    .withMessage("El password no puede ir vacio")
     .run(req);
   await check("repetir_password")
     .equals(req.body.password)
@@ -37,14 +41,52 @@ const registrar = async (req, resp) => {
     return resp.render("auth/registro", {
       pagina: "Crear cuenta",
       errores: resultado.array(),
-      usuario:{
-        nombre:req.body.nombre,
-        email:req.body.email
-      }
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
     });
   }
-  const usuario = await Usuario.create(req.body);
-  resp.json(usuario);
+
+  //Extraer datos
+  const { nombre, email, password } = req.body;
+
+  //Verificar que el usuario existe
+  const existeUsuario = await Usuario.findOne({
+    where: { email },
+  });
+
+  if (existeUsuario) {
+    return resp.render("auth/registro", {
+      pagina: "Crear cuenta",
+      errores: [{ msg: "El usuario ya esta registrado" }],
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email,
+      },
+    });
+  }
+
+  //almacenar en la base de datos
+  const usuario = await Usuario.create({
+    nombre,
+    email,
+    password,
+    token: generarId(),
+  });
+
+  //Enviar email de confirmación
+  emailRegistro({
+    nombre: usuario.nombre,
+    email: usuario.email,
+    token: usuario.token,
+  });
+
+  //Mensaje de confirmación de cuenta
+  resp.render("templates/mensaje", {
+    pagina: "Cuenta creada correctamente",
+    mensaje: "Hemos enviado un email de confirmación, presiona en el enlace",
+  });
 };
 
 const formularioRecuperarPassword = (req, resp) => {
---
+++ helpers/emails.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+const emailRegistro = async (datos) => {
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  })
+
+  const {nombre, email, token} = datos;
+
+  //Enviar email
+  await transport.sendMail({
+    from: 'Bienes raices.com',
+    to:email,
+    subject:'Confirma tu cuenta en bienesraices.com',
+    text:'Confirma tu cuenta en bienesraices.com',
+    html: `
+        <p>Hola ${nombre}, comprueba tu cuenta en bienesRaices.com<p/>
+        <p>Tu cuenta ya esta lista, solo debes confirmar en el siguiente enlace
+            <a href=''>Confirma tu cuenta<a/>
+        <p/>
+        <p>Si tu no creaste la cuenta, puedes hacer caso omiso a este mensaje<p/>
+
+    `
+  })
+
+
+};
+
+export { emailRegistro };
